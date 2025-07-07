6c77d7227a7fc4fe657aa371b5b92df7008c004b
RAMVARSHA27 <ramvarshar.22csd@kongu.edu>
2025-07-07 12:11:43 +0530

    css9

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/my-portfolio/", // ✅ match homepage path
   plugins: [react()],
+  base: '/my-portfolio/' // 👈 VERY IMPORTANT
 })
